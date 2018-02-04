f03011349edebd262333a998fe856ceb2958739e
nekisse <lsh891224@gmail.com>
2018-02-04 17:50:27 +0900

    BaseTimeEntity 추상클래스 추가 @MappedSuperclass JPA Entity 클래스들이 BaseTimeEntity을 상속할 경우 필드들(createdDate, modifiedDate)도 컬럼으로 인식하도록 함
    @EntityListeners(AuditingEntityListener.class)
    BaseTimeEntity클래스에 Auditing 기능을 포함시킴
    
    @CreatedDate
    Entity가 생성되어 저장될 때 시간이 자동 저장
    @LastModifiedDate
    조회한 Entity의 값을 변경할 때 시간이 자동 저장
    
    Posts 클래스에서 BaseTimeEntity 상속하도록 변경
    
    Application 클래스에
    @EnableJpaAuditing       JPA Auditing 활성화
    
    JPA Auditing 테스트 코드 작성.

---
+++ src/main/java/com/nekisse/webservice/domain/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.nekisse.webservice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+
+}
